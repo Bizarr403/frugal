338c16103738adb7e44bc5e54a831979b4ed07c3
PC <ladebosky2019@gmail.com>
2025-07-04 15:51:30 +0100

    Final fixes🤲🏾

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,33 +1,3 @@
-import { handleAuth,getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-const prisma = new PrismaClient();
-
-export const GET = handleAuth({
-  async callback(req) {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
-
-    if (!user || !user.id) {
-      throw new Error("Something went wrong with authentication: " + JSON.stringify(user));
-    }
-
-    let dbUser = await prisma.user.findUnique({
-      where: { kindeId: user.id },
-    });
-
-    if (!dbUser) {
-      dbUser = await prisma.user.create({
-        data: {
-          kindeId: user.id,
-          firstName: user.given_name ?? "",
-          lastName: user.family_name ?? "",
-          email: user.email ?? "",
-        },
-      });
-    }
-
-    return NextResponse.redirect("http://localhost:3000/dashboard");
-  },
-});
+export const GET = handleAuth();
